5f926708cab53fa85b7436749929b2c9e6f67eda
Cora Kingdon <ckingdon@berkeley.edu>
2018-03-02 12:54:55 -0500

    makes runtimestep calculation for MAT in co2cycle one timestep ahead

--- src/components/co2cycle_component.jl
+++ src/components/co2cycle_component.jl
@@ -28,15 +28,6 @@ function run_timestep(state::co2cycle, t::Int)
     v = state.Variables
     p = state.Parameters
 
-    #Define function for MAT
-    if t==1
-        v.MAT[t] = p.mat0
-    elseif t==2
-        v.MAT[t] = p.mat1
-    else
-        v.MAT[t] = v.MAT[t-1] * p.b11 + v.MU[t-1] * p.b21 + (p.E[t-1]*10)
-    end
-
     #Define function for MU
     if t==1
         v.MU[t] = p.mu0
@@ -49,6 +40,21 @@ function run_timestep(state::co2cycle, t::Int)
         v.ML[t] = p.ml0
     else
         v.ML[t] = v.ML[t-1] * p.b33 + v.MU[t-1] * p.b23
+    end
+
+    #Define function for MAT
+    # if t==1
+    #     v.MAT[t] = p.mat0
+    # elseif t==2
+    #     v.MAT[t] = p.mat1
+    # else
+    #     v.MAT[t] = v.MAT[t-1] * p.b11 + v.MU[t-1] * p.b21 + (p.E[t-1]*10)
+    # end
+    if t==1
+        v.MAT[1] = p.mat0
+        v.MAT[2] = p.mat1
+    elseif t < 60
+        v.MAT[t+1] = v.MAT[t] * p.b11 + v.MU[t] * p.b21 + (p.E[t]*10)
     end
 
 end
