7341de750040470d7d68d5906624df87b3741a30
lrennels <lrennels@berkeley.edu>
2018-03-27 17:57:11 -0700

    Update marginaldamage.jl file for new Mimi

--- src/marginaldamage.jl
+++ src/marginaldamage.jl
@@ -1,4 +1,30 @@
-include("dice2013.jl")
+include("dice2010.jl")
+using dice2010
+
+function getmarginal_dice_models(;emissionyear=2010)
+    mm = MarginalModel(DICE)
+    m1 = mm.base
+    m2 = mm.marginal
+
+    addcomponent(m2, adder, :marginalemission, before=:co2cycle)
+
+    time = dimension(m1, :time)
+    addem = zeros(length(time))
+    addem[time[emissionyear]] = 1.0
+
+    set_parameter!(m2, :marginalemission, :add, addem)
+    connect_parameter(m2, :marginalemission, :input, :emissions, :E)
+    connect_parameter(m2, :co2cycle, :E, :marginalemission, :output)
+
+    run(m1)
+    run(m2)
+
+    return m1, m2
+end
+
+#=
+
+Old Way 
 
 function getmarginal_dice_models(;emissionyear=2010)
     m1 = getdiceexcel()
@@ -16,3 +42,4 @@ function getmarginal_dice_models(;emissionyear=2010)
 
     return m1, m2
 end
+ =#
